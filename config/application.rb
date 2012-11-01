a3760dd34f3a2b56f14b72536939890803041c5c
Samuel Garner <garnersamuel@hotmail.com>
2012-11-02 11:54:36 +1300

    User signup complete

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
   end
 end
